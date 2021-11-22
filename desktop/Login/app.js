c7296ed5fbe83dc8f9dfbe9f2e304e5f79b3311e
Mateus <mmreis@rd.com.br>
2021-11-22 13:05:01 -0300

    layouts finalizados

---
+++ desktop/Login/app.js
@@ -0,0 +1,15 @@
+function validar() {
+    var email = formulario.email;
+    if (email.value == "") {
+        alert("E-mail não informado.")
+        email.focus();
+    }
+}
+
+function validar() {
+    var senha = formulario.senha;
+    if (senha.value == "") {
+        alert("Senha não informada.")
+        senha.focus();
+    }
+}
